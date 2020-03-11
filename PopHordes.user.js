3f390effb26cb95e27b0344ad70f43a602c111df
Croaaa <56492252+Croaaa@users.noreply.github.com>
2020-03-11 03:51:11 +0100

    Update PopHordes.user.js

--- PopHordes.user.js
+++ PopHordes.user.js
@@ -13,15 +13,18 @@ var data=false,
     town= {x:0,y:0},
     coord= {x:0,y:0},
     js= unsafeWindow.js,
+    version= 1.5,
     hasINITIALISED= false,
+    theLastURLForBack= false,
     haxe= unsafeWindow.haxe,
     console= unsafeWindow.console,
     infos= unsafeWindow.__tid.infos,
     MapCommon= unsafeWindow.MapCommon,
+    ExploCommon= unsafeWindow.ExploCommon,
     localStorage= unsafeWindow.localStorage,
     decodeURL= t=>decodeURIComponent(t.split("+").join(" "));
 
-class decode{constructor(e){let t=decodeURIComponent(e.split("+").join(" ")),l=MapCommon.genKey(t.length),n=MapCommon.permute(t);this.serial=this.binaryToMessage(l,n)}binaryToMessage(e,t){let l=[];for(let t=0,n=e.length;t<n;t++){let n=this.translate(e.charCodeAt(t));null!=n&&l.push(n)}l=l.length<1?[0]:l;let n="";for(let e=0,o=t.length;e<o;e++){let o=t.charCodeAt(e)^l[(e+t.length)%l.length];n+=String.fromCharCode(0!==o?o:t.charCodeAt(e))}return n}translate(e){return e>=65&&e<=90?e-65:e>=97&&e<=122?e-71:e>=48&&e<=57?e+4:null}}
+class decode{constructor(e,t){let n=decodeURIComponent(e.split("+").join(" ")),o=t?ExploCommon.genKey(n.length):MapCommon.genKey(n.length),l=t?ExploCommon.permute(n):MapCommon.permute(n);this.serial=this.binaryToMessage(o,l)}binaryToMessage(e,t){let n=[];for(let t=0,o=e.length;t<o;t++){let o=this.translate(e.charCodeAt(t));null!=o&&n.push(o)}n=n.length<1?[0]:n;let o="";for(let e=0,l=t.length;e<l;e++){let l=t.charCodeAt(e)^n[(e+t.length)%n.length];o+=String.fromCharCode(0!==l?l:t.charCodeAt(e))}return o}translate(e){return e>=65&&e<=90?e-65:e>=97&&e<=122?e-71:e>=48&&e<=57?e+4:null}}
 class unserializeur{constructor(t){this.buffer=t,this.length=t.length,this.cache=[],this.scache=[],this.pos=0,this.unserialized=this.unserialize()}unserialize(){let a=this.buffer[this.pos++],b={i:"this.readDigits()",o:"this.readObject()",y:"this.readString()"};if(b.hasOwnProperty(a))return eval(b[a]);throw`Invalid char "${this.buffer[this.pos-1]}" (${this.buffer.charCodeAt(this.pos-1)}) at position ${this.pos-1}`}readDigits(){let t=0,i="-"===this.buffer[this.pos]&&(this.pos++,!0);for(;;){let i=this.buffer[this.pos];if(["0","1","2","3","4","5","6","7","8","9"].indexOf(i)<0)break;t=10*t+parseInt(i),this.pos++}return i?-1*t:t}readString(){let t=this.readDigits();if(":"!==this.buffer[this.pos++]||this.length-this.pos<t)throw"Invalid string length";{let i=decodeURL(this.buffer.slice(this.pos,this.pos+=t));return this.scache.push(i),i}}readObject(){let t={};for(;;){if(this.pos>=this.length)throw"Invalid object";if("g"===this.buffer[this.pos])break;{let i=this.unserialize();if(["number","string"].indexOf(typeof i)<0)throw"Invalid object key";{let s=this.unserialize();t[i]=s}}}return this.pos++,this.cache.push(t),t}}
 
 function sel(a,b) {
@@ -35,10 +38,16 @@ function sel(a,b) {
     }
 }
 function getPopupContent() {
-    let text= sel('#notificationText').textContent;
+    if(theLastURLForBack.search('removeFromBag')>0) {
+        return "[DEPOT DESERT]";
+    } else if(theLastURLForBack.search('grabItem')>0) {
+        return "[PRISE DESERT]";
+    } else {
+        let text= sel('#notificationText').textContent;
         text= text.replace(/[\s]/g, ' ');
         text= text.replace(/[ ]{2,}/g, ' ');
-    return text.trim();
+        return text.trim();
+    }
 }
 const allStatus= ["status_hasEaten", "status_hasDrunk", "status_thirst", "status_dehyd", "status_drunk", "status_over", "status_clean", "status_drugged", "status_addict", "small_ghoul", "status_wound", "status_healed", "status_infect", "item_disinfect", "status_tired", "status_terror", "small_camp", "item_shield_mt"];
 // Rassasié, Désaltéré, Soif, Déshydraté, Ivre, Gueule de bois, Clean, Drogué, Dépendant, Goule, Blessé, Soigné, Infecté, Immunisé, Fatigué, Terrorisé, Campeur Avisé, Vaincre la mort
@@ -88,29 +97,52 @@ function imBan() {
     if(a||b||c) return "Y";
     return "N";
 }
+function getCityType() {
+    if (sel('div#clock div.day span.hard')) return "PANDE";
+    else if (data) {
+        if (data._h > 24) return "RE";
+        else if (data._h < 15) return "RNE";
+    }
+    else return "RE/RNE";
+}
 async function init() {
-    let dateHour= new Date();
-        dateHour= `${dateHour.toLocaleString("fr-FR")}.${dateHour.getMilliseconds()}`;
+    let url= this.urlForBack,
+        dateHour= new Date();
+    dateHour= `${dateHour.toLocaleString("fr-FR")}.${dateHour.getMilliseconds()}`;
     let notif = document.getElementById("notification");
-    if(notif.classList.contains("showNotif") && !notif.classList.contains("aspired")) {
+    if(
+        (notif.classList.contains("showNotif") && !notif.classList.contains("aspired"))
+        || (
+            (theLastURLForBack.search('removeFromBag')>0 || theLastURLForBack.search('grabItem')>0)
+            && (Math.abs(coord.x)+Math.abs(coord.y)!=0)
+        )
+    ) {
         notif.classList += " aspired"
         console.log("[POPHORDES] Aspiration Popup en cours ..");
-        let aspire= [
-            `${new unserializeur(infos).unserialized.realId}`,
-            sel('#tid_openRight .tid_name').textContent.trim(),
-            sel('#clock > .name').textContent.trim(),
-            dateHour.slice(0,10),
-            dateHour.slice(13,dateHour.length),
-            sel('#clock > .day').textContent.replace(/[^0-9]/g, ''),
-            sel('#serverTime').textContent.trim(),
-            sel('.counter').textContent.trim(),
-            `${coord.x}`,
-            `${coord.y}`,
-            getSoul(),
-            getPopupContent(),
-            (sel('#FlashExplo')?"Y":"N")
-        ];
-        aspire= aspire.concat(imBan(),getStatus(),getItems(),["","","","","","","","","","","",""]).slice(0,44);
+        let aspire= {
+
+            hordesId: `${new unserializeur(infos).unserialized.realId}`,
+            pseudo: sel('#tid_openRight .tid_name').textContent.trim(),
+            cityName: sel('#clock > .name').textContent.trim(),
+            cityType: getCityType(),
+            cityDay: sel('#clock > .day').textContent.replace(/[^0-9]/g, ''),
+            cityHour: sel('#serverTime').textContent.trim(),
+            realDate: dateHour.slice(0,10),
+            realHour: dateHour.slice(13,dateHour.length),
+            ap: sel('.counter').textContent.trim(),
+            coordX: `${coord.x}`,
+            coordY: `${coord.y}`,
+            soulPosition: getSoul(),
+            popupContent: getPopupContent(),
+            onBuilding: (sel('.outSpot h2')?sel('.outSpot h2').textContent.trim():"N"),
+            driedZone: (sel('.driedZone')?"Y":"N"),
+            inRuin: (sel('#FlashExplo')?"Y":"N"),
+            imBan: imBan(),
+            listStatus: getStatus(),
+            listItem: getItems().concat(["","","","","","","","","","","",""]).slice(0,12),
+            scriptVersion: version
+        };
+
         console.log(aspire);
         let localSTR= localStorage.getItem('popHordesCache'),
             dataArray= [];
@@ -134,13 +166,13 @@ async function init() {
         }).then(a=>a.json());
         if(true) {
             localStorage.removeItem('popHordesCache');
-        }
+         }
     }
 }
 function initMap() {
     if(!hasINITIALISED&&sel('#FlashMap')) {
         let d= sel('#FlashMap').getAttribute('flashvars').slice(13);
-            d= haxe.Unserializer.run(new decode(d).serial);
+        d= haxe.Unserializer.run(new decode(d, false).serial);
         data= d;
         let ville= {x:0,y:0};
         for(let i=0;i<d._details.length;i++) {
@@ -164,12 +196,12 @@ function urlToObj(a) {
 }
 
 (function() {
-    init();
+    //init();
     js.XmlHttp.pophordesOnEnd= js.XmlHttp.onEnd;
     js.XmlHttp.onEnd= function() {
-        let url= this.urlForBack;
-        if(url&&url.startsWith('outside/go?')) {
-            let i= urlToObj(url.split('?')[1]);
+        theLastURLForBack= this.urlForBack;
+        if(theLastURLForBack&&theLastURLForBack.startsWith('outside/go?')) {
+            let i= urlToObj(theLastURLForBack.split('?')[1]);
             coord.x+= parseInt(i.x);
             coord.y-= parseInt(i.y);
         }
