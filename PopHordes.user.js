8151c788f78feb6f61644d96a0e956c18f9716a7
Croaaa <56492252+Croaaa@users.noreply.github.com>
2020-04-14 23:06:38 +0200

    Update PopHordes.user.js

--- PopHordes.user.js
+++ PopHordes.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name            PopEnd
+// @name            PopHordes
 // @description     
 // @match           http*://www.hordes.fr/*
 // @icon            http://data.hordes.fr/gfx/icons/item_cards.gif
